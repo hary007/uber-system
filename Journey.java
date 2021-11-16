e445270e24f66c947b4c18e700c9ecd12cfaaab3
Hary Ahuja <haryahuja07@gmail.com>
2021-11-17 00:59:12 +0530

    Added Cab and Driver Classes

--- Journey.java
+++ Journey.java
@@ -27,6 +27,100 @@ class City {
     }
 }
 
+class Landmark extends City {
+    String name;
+    private int abscissa;
+    private int ordinate;
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setAbscissa(int a) {
+        abscissa = a;
+    }
+
+    void setOrdinate(int b) {
+        ordinate = b;
+    }
+
+    int getAbscissa() {
+        return this.abscissa;
+    }
+
+    int getOrdinate() {
+        return this.ordinate;
+    }
+
+}
+
+class Cab {
+    private String registerationNumber;
+    String carType;
+
+    void setRegNumber(String registerationNumber) {
+        this.registerationNumber = registerationNumber;
+    }
+
+    void setCarType(String carType) {
+        this.carType = carType;
+    }
+}
+
+class Driver extends Cab {
+    private String name;
+    private float rating;
+    Landmark currentLocation;
+    boolean available = true; // this variable will be true if a driver is ready to pick the customer up.
+    int speed;
+
+    Driver() {
+
+    }
+
+    Driver(String name, float rating, Landmark currentLocation)
+
+    void changeAvailableStatus() {
+        if (available)
+            available = false;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    float getRating() {
+        return this.rating;
+    }
+
+    void setCurrentLocation(Landmark l) {
+        currentLocation = l;
+    }
+
+    void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    void startRide() {
+        System.out.println("Your ride has started!");
+    }
+
+    void endRide() {
+        System.out.println("You have reached your destination. Thank You!");
+        // driver will become available again
+        // the destination location of customer will become the current location of
+        // driver. 1) take parameter in this method. 2) Use journey class
+    }
+}
+
 class Journey {
 
     public static void main(String[] args) {
