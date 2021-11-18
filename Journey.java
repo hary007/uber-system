4d3118daedf3c97ce815e5130c7a54a2b593d81e
Hary Ahuja <haryahuja07@gmail.com>
2021-11-18 16:15:00 +0530

    minor changes

--- Journey.java
+++ Journey.java
@@ -34,6 +34,15 @@ class Landmark extends City {
     private int abscissa;
     private int ordinate;
 
+    Landmark() {
+    };
+
+    Landmark(Landmark l) {
+        this.name = l.name;
+        this.abscissa = l.abscissa;
+        this.ordinate = l.ordinate;
+    }
+
     void setName(String name) {
         this.name = name;
     }
@@ -73,22 +82,25 @@ class Driver extends Cab {
     private String name;
     private float rating;
     Landmark currentLocation;
-    boolean available = true; // this variable will be true if a driver is ready to pick the customer up.
+    int available; // this variable will be true if a driver is ready to pick the customer up.
+    // how to change the value of available in the database once the ride has been
+    // confirmed.
     int speed;
 
     Driver() {
 
     }
 
-    Driver(String name, float rating, Landmark currentLocation) {
+    Driver(String name, float rating, Landmark currentLocation, int available) {
         this.name = name;
         this.rating = rating;
         this.currentLocation = currentLocation;
+        this.available = available;
     }
 
     void changeAvailableStatus() {
-        if (available)
-            available = false;
+        if (available == 1)
+            available = 0;
     }
 
     void setName(String name) {
@@ -126,45 +138,52 @@ class Driver extends Cab {
         // driver. 1) take parameter in this method. 2) Use journey class
     }
 }
-class Customer extends Landmark{
+
+class Customer extends Landmark { // why is customer extending landmark
     String name;
     Landmark pickupLocation;
     Landmark destination;
-    void chooseDriver(){
-        //return list of available drivers from the database
+
+    void chooseDriver() {
+        // return list of available drivers from the database
     }
-    void choosePickupLocation(){
+
+    void choosePickupLocation() {
         System.out.println("Enter pickup Location");
-        Scanner s=new Scanner(System.in);
-        int x=s.nextInt();
-        int y=s.nextInt();
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
         s.close();
         pickupLocation.setAbscissa(x);
         pickupLocation.setOrdinate(y);
     }
-    void choosedestination(){
+
+    void choosedestination() {
         System.out.println("Enter pickup Location");
-        Scanner s=new Scanner(System.in);
-        int x=s.nextInt();
-        int y=s.nextInt();
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
         s.close();
         destination.setAbscissa(x);
         destination.setOrdinate(y);
     }
-    void cancelBooking(Driver d){
-        //driver available status to be changed.
-        d.available=true;
+
+    void cancelBooking(Driver d) {
+        // driver available status to be changed.
+        d.available = 1;
         System.out.println("The booking is cancelled");
     }
 }
 
 class Journey {
 
+    static List<Driver> drivers;
+    static List<Landmark> landmarks;
+
     public static void main(String[] args) {
 
-
         Scanner sc = null;
-        try {
+        try { // user input inside try or not
             sc = new Scanner(new FileReader("city.txt"));
 
             int n = sc.nextInt();
@@ -173,7 +192,7 @@ class Journey {
 
             int t = sc.nextInt();
 
-            List<Landmark> landmarks = new ArrayList<Landmark>();
+            landmarks = new ArrayList<Landmark>();
 
             for (int i = 0; i < t; i++) {
                 int x = sc.nextInt();
@@ -189,45 +208,37 @@ class Journey {
             }
 
             // for (int i = 0; i < t; i++) {
-            //     System.out.println(landmarks.get(i).name);
+            // System.out.println(landmarks.get(i).name);
             // }
 
-            
-        System.out.println("Enter pickup Location:-");
-        Scanner s=new Scanner(System.in);
+            System.out.println("Enter pickup Location:-");
+            Scanner s = new Scanner(System.in);
 
-        String pickupLocation;
-        pickupLocation=s.next();
-        int pickupX,pickupY;
-        
-        System.out.println("Enter destination:-");
+            String pickupLocation;
+            pickupLocation = s.next(); // keep iterating till right location is not entered
+            int pickupX, pickupY;
 
-        String destination;
-        int destX,destY;
+            System.out.println("Enter destination:-");
 
-        destination=s.next();
-        for(int i=0;i<landmarks.size();i++){
-            if(pickupLocation==landmarks.get(i).name){
-                destX=landmarks.get(i).getAbscissa();
-                destY=landmarks.get(i).getOrdinate();
+            String destination;
+            int destX, destY;
+
+            destination = s.next();
+            for (int i = 0; i < landmarks.size(); i++) {
+                if (pickupLocation == landmarks.get(i).name) {
+                    destX = landmarks.get(i).getAbscissa();
+                    destY = landmarks.get(i).getOrdinate();
+                } else if (destination == landmarks.get(i).name) {
+                    pickupX = landmarks.get(i).getAbscissa();
+                    pickupY = landmarks.get(i).getOrdinate();
+                }
             }
-            else if(destination==landmarks.get(i).name){
-                pickupX=landmarks.get(i).getAbscissa();
-                pickupY=landmarks.get(i).getOrdinate();
-            }
-        }
-
-
-
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getStackTrace());
         } finally {
             sc.close();
         }
-
-        
-        
 
     }
 }
